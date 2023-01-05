33e98eafb282ebb82d1a9f78ead023f2a3f5fdee
Shahnia <khanshahnia@gmail.com>
2023-01-04 23:01:28 -0500

    Added String filename at the question class

--- Question.java
+++ Question.java
@@ -9,25 +9,20 @@ import java.util.Random;
 public class Question {
     static FileReader file;
     static BufferedReader buffer;
-    public static void randomQuestion() throws IOException {
-//        try {
-//            file = new FileReader("easy.txt"); // Initializes  the FileReader
-//            buffer = new BufferedReader(file); // Initializes the BufferedReader
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
+    public static void randomQuestion(String filename) throws IOException {
+
         Random rn = new Random();
         int i = (rn.nextInt(3))  * 7;
         for (int l = 0; l < 5; l++) {
             String line = Files.readAllLines(Paths.get("easy.txt")).get(i+l);
-         //   String line33 = Files.readAllLines(Paths.get("easy.txt")).get(i + 1);
+
             System.out.println(line);
-           // System.out.println(line33);
+
         }
     }
 
     public static void main(String[] args) throws IOException {
-        randomQuestion();
+        randomQuestion("medium.txt");
     }
 
 
