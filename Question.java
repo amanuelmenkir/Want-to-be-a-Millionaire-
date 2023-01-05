36d7afc98bee39cf4e0f9aa6d3f1eb942567297f
Shahnia <khanshahnia@gmail.com>
2023-01-04 23:25:49 -0500

    Added Scanner to take userInput

--- Question.java
+++ Question.java
@@ -5,16 +5,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
-
+import java.util.Scanner;
 public class Question {
     static FileReader file;
     static BufferedReader buffer;
+
+
+
+
     public static void randomQuestion(String filename) throws IOException {
 
         Random rn = new Random();
         int i = (rn.nextInt(3))  * 7;
         for (int l = 0; l < 5; l++) {
-            String line = Files.readAllLines(Paths.get("easy.txt")).get(i+l);
+            String line = Files.readAllLines(Paths.get(filename)).get(i+l);
 
             System.out.println(line);
 
@@ -22,7 +26,15 @@ public class Question {
     }
 
     public static void main(String[] args) throws IOException {
-        randomQuestion("medium.txt");
+
+        randomQuestion("easy.txt");
+        //randomQuestion("medium.txt");
+       // randomQuestion("hard.txt");
+        Scanner userInput = new Scanner (System.in);
+        System.out.println("Enter your answer: ");
+        String inout  = userInput.nextLine();
+
+        
     }
 
 
